fba59050a67a576f73edf0f75715907862f70a06
markete86 <markete86@gmail.com>
2021-02-15 14:58:06 -0500

    tratando de pasar el list cart para un componente

--- src/components/Buscador.js
+++ src/components/Buscador.js
@@ -5,57 +5,34 @@ import ProductList from "./ProductList";
 import ShoppingCart from "./ShoppingCart";
 
 
- const Buscador =()=>{
+ const Buscador =(props)=>{
+    const searchRef = React.createRef();
+    const handleSubmitSearch =(e)=>{
+         e.preventDefault();
+         const termino = searchRef.current.value;
+         props.dataSearch(termino);
+
+     }
+     const restore = ()=>{
+         props.restoreTodo();
+     }
      return (
-         <form onSubmit={this.handleSubmitSearch}>
+         <form onSubmit={()=>handleSubmitSearch(e)}>
              <div className="row">
                  <div className="form-group col-md-8">
-                     <input ref={this.searchRef} type="text" className="form-control form-control-lg border-primary"
+                     <input ref={searchRef} type="text" className="form-control form-control-lg border-primary"
                             placeholder="busca el producto..."/>
                  </div>
                  <div className="form-group col-md-2">
                      <input type="submit" className="btn btn-lg btn-danger btn-block" value="Buscar"/>
                  </div>
                  <div className="form-group col-md-2">
-                     <button type="button" onClick={this.restore} className="btn btn-lg btn-info btn-block">Limpiar</button>
+                     <button type="button" onClick={()=>restore()} className="btn btn-lg btn-info btn-block">Limpiar</button>
                  </div>
              </div>
          </form>
      )
  }
 
-
-//Old buscador
-class app extends Component {
-    searchRef = React.createRef();
-    handleSubmitSearch =(e)=>{
-        e.preventDefault();
-        const termino = this.searchRef.current.value;
-        this.props.dataSearch(termino);
-    }
-    restore = ()=>{
-        this.props.restoreTodo();
-    }
-
-    render() {
-        return (
-            <form onSubmit={this.handleSubmitSearch}>
-                <div className="row">
-                    <div className="form-group col-md-8">
-                        <input ref={this.searchRef} type="text" className="form-control form-control-lg border-primary"
-                               placeholder="busca el producto..."/>
-                    </div>
-                    <div className="form-group col-md-2">
-                        <input type="submit" className="btn btn-lg btn-danger btn-block" value="Buscar"/>
-                    </div>
-                    <div className="form-group col-md-2">
-                        <button type="button" onClick={this.restore} className="btn btn-lg btn-info btn-block">Limpiar</button>
-                    </div>
-                </div>
-            </form>
-        )
-    }
-
-}
 
 export default Buscador;
